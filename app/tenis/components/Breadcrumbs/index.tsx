a9afc3b5dbe32627e8a975fa60d66b7540ea68c1
dudow <edu30.nm@gmail.com>
2025-05-14 14:41:53 -0300

    feat: create Breadcrumbs component

---
+++ app/tenis/components/Breadcrumbs/index.tsx
@@ -0,0 +1,10 @@
+// Mocked Breadcrumbs
+
+export const Breadcrumbs = () => {
+  return (
+    <div className="mb-8 mt-3 text-sm hidden md:flex gap-2">
+      <a href="#">Página Inicial</a> / <a href="#">Masculino</a> /{' '}
+      <a href="#">Calçados</a> / <a href="#">Tênis</a>
+    </div>
+  );
+};
