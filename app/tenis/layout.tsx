822f85a9c88696c397bebfb624687f447107e30f
dudow <edu30.nm@gmail.com>
2025-05-14 14:41:17 -0300

    feat: add metadata

--- app/tenis/layout.tsx
+++ app/tenis/layout.tsx
@@ -1,3 +1,6 @@
+import '../globals.css';
+
+import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -28,13 +31,15 @@ export const metadata: Metadata = {
   },
 };
 
-export default function ProductPageLayout({
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className={inter.variable}>
       <body className="font-sans bg-white text-zinc-900">{children}</body>
     </html>
   );
